08c446abcfac1fb713a0e7e792037215f4a20996
Dandi Darmawan <dandi.darmawan@eventx.io>
2026-05-07 01:34:54 +0700

    Fetch file content from head SHA for accurate line numbers

--- src/review.ts
+++ src/review.ts
@@ -172,7 +172,7 @@ export const codeReview = async (
             owner: repo.owner,
             repo: repo.repo,
             path: file.filename,
-            ref: context.payload.pull_request.base.sha
+            ref: context.payload.pull_request.head.sha
           })
           if (contents.data != null) {
             if (!Array.isArray(contents.data)) {
